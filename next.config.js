c71a619b29f546b5b087dda5df6b8c80b447e8a4
Bogdan Dolin <d.bogdan@inbox.ru>
2022-07-08 19:20:05 +0700

    Move GitHub token to .env.local

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    GITHUB_TOKEN: "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",
-  },
 };
 
 module.exports = nextConfig;
