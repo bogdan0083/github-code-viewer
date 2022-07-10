33d4869b92736b306a49c9afaf908288d1cec35a
Bogdan Dolin <d.bogdan@inbox.ru>
2022-07-10 17:49:03 +0700

    Implement Select component /is

---
+++ components/form/Select/Select.tsx
@@ -0,0 +1,57 @@
+import { ChangeEvent } from "react";
+
+type Rec = Record<string, string>;
+type SelectOptions = Record<string, string | Rec>;
+
+interface SelectProps {
+  onChange: (selected: string[]) => void;
+  multiple?: boolean;
+  options: SelectOptions;
+  placeholder?: string;
+}
+
+const Select = ({ onChange, multiple, options, placeholder }: SelectProps) => {
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (multiple) {
+      const values = [...e.target.options]
+        .filter((option) => option.selected)
+        .map((option) => option.value);
+      onChange(values);
+    } else {
+      const { value } = e.target;
+      if (value) {
+        onChange([value]);
+      }
+    }
+  };
+
+  const renderOptions = (
+    opts: SelectOptions
+  ): (JSX.Element | JSX.Element[])[] => {
+    const optionsList = Object.entries(opts);
+    return optionsList.map(([key, value]) => {
+      if (typeof value === "string") {
+        return (
+          <option key={key} value={key}>
+            {value}
+          </option>
+        );
+      } else {
+        return (
+          <optgroup key={key} label={key}>
+            {renderOptions(value)}
+          </optgroup>
+        );
+      }
+    });
+  };
+
+  return (
+    <select onChange={handleChange} multiple={multiple}>
+      {placeholder && <option value="">{placeholder}</option>}
+      {renderOptions(options)}
+    </select>
+  );
+};
+
+export default Select;
