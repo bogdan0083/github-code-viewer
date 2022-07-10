eccb873290930cfe7723e0f4d9accef63a2f57bf
Bogdan Dolin <d.bogdan@inbox.ru>
2022-07-10 17:44:27 +0700

    Remove unnecessary util

--- utils/oneWeekAgo.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Get one week ago date in format YYYY-MM-DD
- */
-export function oneWeekAgo(): string {
-  const date = new Date();
-  date.setDate(date.getDate() - 7);
-  return date.toISOString().split("T")[0];
-}
