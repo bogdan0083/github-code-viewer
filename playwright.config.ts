51e0ecb4122b0db986798810061d2f93b551fd35
Bogdan Dolin <d.bogdan@inbox.ru>
2022-08-21 17:56:30 +0700

    Switch to headless firefox for now

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,8 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   testDir: "./__tests__/e2e",
   use: {
-    headless: false,
+    headless: true,
+    browserName: "firefox",
     baseURL: "http://localhost:3000",
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
