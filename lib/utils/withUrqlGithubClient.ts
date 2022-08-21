cac7f032fb6be8d88c9aa783a41c214ca0feef00
Bogdan Dolin <d.bogdan@inbox.ru>
2022-08-21 17:47:31 +0700

    Uncomment relayPagination

--- lib/utils/withUrqlGithubClient.ts
+++ lib/utils/withUrqlGithubClient.ts
@@ -5,6 +5,7 @@ import { devtoolsExchange } from "@urql/devtools";
 import { dedupExchange, errorExchange, fetchExchange } from "urql";
 import { cacheExchange } from "@urql/exchange-graphcache";
 import { retryExchange } from "@urql/exchange-retry";
+import { relayPagination } from "@urql/exchange-graphcache/extras";
 
 export const graphqlGithubClientConfig = {
   url: GITHUB_GRAPHQL_URL,
@@ -17,11 +18,11 @@ export const graphqlGithubClientConfig = {
     devtoolsExchange,
     dedupExchange,
     cacheExchange({
-      // resolvers: {
-      //   Query: {
-      //     search: relayPagination(),
-      //   },
-      // },
+      resolvers: {
+        Query: {
+          search: relayPagination(),
+        },
+      },
       keys: {
         // @ts-ignore
         TreeEntry: (entry) => entry.path || null,
